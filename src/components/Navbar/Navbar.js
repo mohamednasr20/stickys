cf6a824c0fc367cf1bbb07816995e41c52e82231
mohamednasr20 <mohamednasr8687@gmail.com>
2021-12-28 13:08:30 -0500

    add navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+import { Container, Nav, Navbar, Button } from 'react-bootstrap';
+import './navbar.css';
+
+const MyNavbar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand className="brand" href="#home">
+          <img src={logo} alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">OUR MENU</Nav.Link>
+            <Nav.Link href="#link">LOCATIONS</Nav.Link>
+            <Nav.Link href="#link">ABOUT US</Nav.Link>
+            <Nav.Link href="#link">CATERING</Nav.Link>
+          </Nav>
+          <Button className="p-0" variant="secondary">
+            <Nav.Link href="#link">CATERING</Nav.Link>
+          </Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default MyNavbar;
